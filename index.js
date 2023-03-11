2c33efaaa5bb47e7213fcafbc0575518d197ad33
Overlord <oleg.tolstov.02@gmail.com>
2023-03-11 20:30:21 +0200

    2

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./db/db.json');
 const cors = require('cors');
-
 const PORT = process.env.PORT || 5000;
 
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
 server.use(router);
-server.use(cors());
+server.use(cors({
+  origin: true,
+  credentials: true,
+  preflightContinue: false,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+}));
+
+server.options('*', cors());
 
 server.listen(PORT, () => {
   console.log('Server is running');
