06da4104c5a3f34ac1a4f2ea354deed0342e2116
Overlord <oleg.tolstov.02@gmail.com>
2023-03-11 19:56:14 +0200

    1

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./db/db.json');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
 server.use(router);
+server.use(cors());
 
 server.listen(PORT, () => {
   console.log('Server is running');
